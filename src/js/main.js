e33c10b22304de2b5fea752d4c94860c9da1e833
mee20004 <mee20004@byui.edu>
2026-01-17 10:26:39 -0700

    NPS part 2

--- src/js/main.js
+++ src/js/main.js
@@ -2,20 +2,7 @@ import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
 
-// Disclaimer link
-const disclaimerLink = document.querySelector(".disclaimer a");
-disclaimerLink.href = parkData.url;
-disclaimerLink.textContent = parkData.fullName;
-
-// Page title
-document.title = parkData.fullName;
-
-// Hero image
-const heroImage = document.querySelector(".hero-banner img");
-heroImage.src = parkData.images[0].url;
-heroImage.alt = parkData.images[0].altText;
-
-// Hero text (name, designation, states)
+// Header
 function parkInfoTemplate(info) {
   return `
     <a href="/" class="hero-banner__title">${info.name}</a>
@@ -26,10 +13,110 @@ function parkInfoTemplate(info) {
   `;
 }
 
-const heroContent = document.querySelector(".hero-banner__content");
+function setHeaderInfo(data) {
+  // Disclaimer link
+  const disclaimerLink = document.querySelector(".disclaimer a");
+  disclaimerLink.href = data.url;
+  disclaimerLink.textContent = data.fullName;
 
-heroContent.innerHTML = parkInfoTemplate({
-  name: parkData.name,
-  designation: parkData.designation,
-  states: parkData.states,
-});
+  // Page title
+  document.querySelector("head > title").textContent = data.fullName;
+
+  // Hero image
+  const heroImage = document.querySelector(".hero-banner img");
+  heroImage.src = data.images[0].url;
+  heroImage.alt = data.images[0].altText || data.fullName;
+
+  // Hero text
+  const heroContent = document.querySelector(".hero-banner__content");
+  heroContent.innerHTML = parkInfoTemplate(data);
+}
+
+// Intro
+function setParkIntro(data) {
+  const introEl = document.querySelector(".intro");
+  introEl.innerHTML = `
+    <h1>${data.fullName}</h1>
+    <p>${data.description}</p>
+  `;
+}
+
+// Info Cards
+function mediaCardTemplate(info) {
+  return `
+    <div class="media-card">
+      <a href="${info.link}">
+        <img src="${info.image}" alt="${info.name}" class="media-card__img">
+        <h3 class="media-card__title">${info.name}</h3>
+      </a>
+      <p>${info.description}</p>
+    </div>
+  `;
+}
+
+const parkInfoLinks = [
+  {
+    name: "Current Conditions &#x203A;",
+    link: "conditions.html",
+    image: parkData.images[2].url,
+    description:
+      "See what conditions to expect in the park before leaving on your trip!"
+  },
+  {
+    name: "Fees and Passes &#x203A;",
+    link: "fees.html",
+    image: parkData.images[3].url,
+    description: "Learn about the fees and passes that are available."
+  },
+  {
+    name: "Visitor Centers &#x203A;",
+    link: "visitor_centers.html",
+    image: parkData.images[9].url,
+    description: "Learn about the visitor centers in the park."
+  }
+];
+
+function setParkInfoLinks(data) {
+  const infoEl = document.querySelector(".info");
+  const html = data.map(mediaCardTemplate);
+  infoEl.innerHTML = html.join("");
+}
+
+// Footer
+function getMailingAddress(addresses) {
+  return addresses.find((address) => address.type === "Mailing");
+}
+
+function getVoicePhone(numbers) {
+  const voice = numbers.find((number) => number.type === "Voice");
+  return voice.phoneNumber;
+}
+
+function footerTemplate(info) {
+  const mailing = getMailingAddress(info.addresses);
+  const voice = getVoicePhone(info.contacts.phoneNumbers);
+
+  return `
+    <section class="contact">
+      <h3>Contact Info</h3>
+      <h4>Mailing Address:</h4>
+      <div>
+        <p>${mailing.line1}</p>
+        <p>${mailing.city}, ${mailing.stateCode} ${mailing.postalCode}</p>
+      </div>
+      <h4>Phone:</h4>
+      <p>${voice}</p>
+    </section>
+  `;
+}
+
+function setFooter(data) {
+  const footerEl = document.querySelector("#park-footer");
+  footerEl.innerHTML = footerTemplate(data);
+}
+
+// Run
+setHeaderInfo(parkData);
+setParkIntro(parkData);
+setParkInfoLinks(parkInfoLinks);
+setFooter(parkData);
